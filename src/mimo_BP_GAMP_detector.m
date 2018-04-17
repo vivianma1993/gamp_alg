ecdf780e1a6cdeb372d1216415b7976ef0752d53
zgd <zgd_521@yahoo.com>
2018-04-17 23:21:08 +0200

    update code

--- src/mimo_BP_GAMP_detector.m
+++ src/mimo_BP_GAMP_detector.m
@@ -1,90 +1,119 @@
 function[LLRs] = mimo_BP_GAMP_detector(y,H_channel,noise_var,const_pointsSymbol,const_pointsLabel,Niter)
 % BP detector based Markov Random Field
-[Nr,Nt] = size(H_channel);
-n_VN = Nt;
-n_FN = Nr;
-FN_edges = nchoosek(1:n_VN,n_VN);
-nb = log2(numel(const_pointsSymbol));
-% LLRs = zeros(Nt,nb,Niter+1);% Number of iteration
- LLRs = zeros(Nt,Niter+1);
+%   1. miu_z_i_k <- sum(h_i_j * E_x_i)
+%   2. o_z_i_k <- sum(|h_i_j|^2 Var(x_j)) + o^2
+%   3. v_i_k <- 4/o_z_i_k^2 * R(h_i_k* (y_i - miu_z_i_k))
+%   4. p_i_k_plus <- exp( sum(l!=i v_l_k) ) / ( 1+ exp( sum(l!=i v_l_k) ))
+%
+% Inputs:
+%   x: s
+%   y: r
+%   H_channel: H_channel
+%   E: 0
+%   ni: n
+%   o: noise_var
+% -----------------------------
+%   y,
+%   H_channel,
+%   noise_var,
+%   const_pointsSymbol,
+%   const_pointsLabel,
+%   Niter
+%
+% Outputs:
+%   xxxxx
+%
 
-% create variable nodes
-for iVN = 1:n_VN
-    VN{iVN,:} =  variable_node_GAMP(iVN);
-end
+    [Nr,Nt] = size(H_channel);
+    n_VN = Nt;
+    n_FN = Nr;
+    FN_edges = nchoosek(1:n_VN,n_VN);
+    nb = log2(numel(const_pointsSymbol));
+    % LLRs = zeros(Nt,nb,Niter+1);% Number of iteration
+    LLRs = zeros(Nt,Niter+1);
 
-% create Function nodes
-for iFN = 1:n_FN
-    nFNIdx = n_VN+iFN; % 给与指定VN相连的FN编号
-    FN{iFN,:} =  function_node_GAMP(nFNIdx);
-end
+    % --------------------------------------------------------------------------
+    % create variable nodes
+    for iVN = 1:n_VN
+        VN{iVN,:} =  variable_node_GAMP(iVN);
+    end
 
-% establish connections between VN and FN
-for iVN = 1:n_VN
-    VN{iVN}.setup_link(FN(iVN,:)');
-end
+    % --------------------------------------------------------------------------
+    % create Function nodes
+    for iFN = 1:n_FN
+        nFNIdx = n_VN+iFN; % ???指??VN?????N??
+        FN{iFN,:} =  function_node_GAMP(nFNIdx);
+    end
 
-% Link connection Function node
-for iFN = 1:n_FN
-    FN{iFN}.setup_link(VN(FN_edges(1,:))');
-end
+    % --------------------------------------------------------------------------
+    % establish connections between VN and FN
+    for iVN = 1:n_VN
+        VN{iVN}.setup_link(FN(iVN,:)'); %'
+    end
 
-nb = log2(numel(const_pointsSymbol));
-ConstLabels_bits = de2bi(const_pointsLabel,nb);
-initMsg = zeros(n_VN,1);
-mean_0 = zeros(n_VN,1);
-var_GAMP_0 = ones(n_VN,1); %?
-s_init = 0;
-% pro_x = ones(n_VN,1);
+    % --------------------------------------------------------------------------
+    % Link connection Function node
+    for iFN = 1:n_FN
+        FN{iFN}.setup_link(VN(FN_edges(1,:))'); %'
+    end
 
-% compute the marginal prob. using joint pdf
-for i = 1 : n_FN
-    for j = 1: n_VN
-        if (i == j)
-            continue ;
+    nb = log2(numel(const_pointsSymbol));
+    ConstLabels_bits = de2bi(const_pointsLabel,nb);
+    initMsg = zeros(n_VN,1);
+    mean_0 = zeros(n_VN,1);
+    var_GAMP_0 = ones(n_VN,1); %?
+    s_init = 0;
+    % pro_x = ones(n_VN,1);
+
+    % --------------------------------------------------------------------------
+    % compute the marginal prob. using joint pdf
+    for i = 1 : n_FN
+        for j = 1: n_VN
+            if (i == j)
+                continue ;
+            end
+            mean_0(i,:) = mean_0(i,:) + H_channel(i,j) * mean_0(i,:);
+            var_GAMP_0(i,:) = abs(H_channel(i,j)'*H_channel(i,j)) * var_GAMP_0(i,:); %'
         end
-        mean_0(i,:) = mean_0(i,:) + H_channel(i,j) * mean_0(i,:);
-        var_GAMP_0(i,:) = abs(H_channel(i,j)'*H_channel(i,j)) * var_GAMP_0(i,:);
+        var_GAMP = var_GAMP_0 + noise_var ;
+        initMsg(i,:) = 4 / var_GAMP(i,:) * (real(conj( H_channel(i,i))*(y(i)-mean_0(i,:))));
     end
-    var_GAMP = var_GAMP_0 + noise_var ;
-    initMsg(i,:) = 4 / var_GAMP(i,:) * (real(conj( H_channel(i,i))*(y(i)-mean_0(i,:))));
-end
-% for i = 1 : n_FN
-%     for j = 1: n_VN
-%         if (i == j)
-%             continue ;           
-%         end
-%         s_init = s_init + initMsg(j,:);
-%     end
 
-for  iVN = 1:n_VN
-    VN{iVN}.setup_init_msg(initMsg(iVN,:));
-end
+    % for i = 1 : n_FN
+    %     for j = 1: n_VN
+    %         if (i == j)
+    %             continue ;
+    %     end
+    %     s_init = s_init + initMsg(j,:);
+    % end
 
-
-% %setup first block of receive signal
-% for  iFN = 1:n_FN
-%     FN{iFN}.ReceivedSignal = {};
-% end
-
-
-LLRs(:,1) = initMsg;
-for iter = 1:Niter
-    %Update Function node
-    for Ln = 1:n_FN
-        FN{Ln}.update_node({y,H_channel,mean_0,var_GAMP});
+    for  iVN = 1:n_VN
+        VN{iVN}.setup_init_msg(initMsg(iVN,:));
     end
-   
-    %update variable node
-    for Ln = 1:n_VN
-        VN{Ln}.update_node({y,H_channel,mean_0,var_GAMP});
+
+    % --------------------------------------------------------------------------
+    % setup first block of receive signal
+    % for  iFN = 1:n_FN
+    %     FN{iFN}.ReceivedSignal = {};
+    % end
+
+    % --------------------------------------------------------------------------
+    LLRs(:,1) = initMsg;
+    for iter = 1:Niter
+        %Update Function node
+        for Ln = 1:n_FN
+            FN{Ln}.update_node({y,H_channel,mean_0,var_GAMP});
+        end
+
+        %update variable node
+        for Ln = 1:n_VN
+            VN{Ln}.update_node({y,H_channel,mean_0,var_GAMP});
+        end
+
+        %SaveLLR for each iteration
+        for nVN = 1:n_VN
+            LLRs(nVN,:,iter+1) = initMsg(nVN,:)+ sum(cell2mat(VN{nVN}.inbound_msg));
+        end
     end
-    
-    %SaveLLR for each iteration
-    
-    for nVN = 1:n_VN
-        LLRs(nVN,:,iter+1) = initMsg(nVN,:)+ sum(cell2mat(VN{nVN}.inbound_msg));
-    end
-end
 
 end
